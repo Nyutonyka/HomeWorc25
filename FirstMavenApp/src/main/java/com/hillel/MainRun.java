9cf31aff0b6e92fecd976c7a25ce662454167476
nyuto <nuytonyka@gmail.com>
2022-11-20 19:22:49 +0200

    Add working with MAVEN

---
+++ FirstMavenApp/src/main/java/com/hillel/MainRun.java
@@ -0,0 +1,9 @@
+package com.hillel;
+
+public class MainRun {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello!!!");
+    }
+}
