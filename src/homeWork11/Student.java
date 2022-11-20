ce7d927e25bcfe0ea4ea1e0ced24313bd44a4cd2
nyuto <nuytonyka@gmail.com>
2022-11-20 11:36:11 +0200

    Add working with lombok

---
+++ src/homeWork11/Student.java
@@ -0,0 +1,22 @@
+package homeWork11;
+
+import lombok.*;
+import lombok.experimental.Accessors;
+
+//@Data содержит в себе: @Getter @Setter @EqualsAndHashCode @ToString
+// и конструктор по умолчанию
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Accessors(chain = true)
+public class Student {
+    private long id;
+    private String name;
+    @EqualsAndHashCode.Exclude
+    private String lastName;
+    @ToString.Exclude
+    private int age;
+}
---
+++ src/homeWork11/StudentRun.java
@@ -0,0 +1,25 @@
+package homeWork11;
+
+public class StudentRun {
+
+    public static void main(String[] args) {
+
+        Student st = new Student();
+        st.setId(1);
+        st.setName("Ann");
+        st.setLastName("Daniel");
+        st.setAge(21);
+
+        System.out.println(st);
+
+        //можно сократить создание класса до одной строчки
+        //с помощью @Accessors(chain = true)
+
+        Student stA = new Student()
+                .setId(2).setName("Ann").setLastName("Daniel").setAge(25);
+
+        System.out.println(stA);
+
+        System.out.println(st.equals(stA));
+    }
+}
