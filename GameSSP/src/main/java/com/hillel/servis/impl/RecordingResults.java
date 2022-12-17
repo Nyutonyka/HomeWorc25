66cc8063e345e0311f99cd80001d0a19151468b2
nyuto <nuytonyka@gmail.com>
2022-12-17 10:59:00 +0200

    Fixing homework GameSSP

--- GameSSP/src/main/java/com/hillel/servis/impl/RecordingResults.java
+++ GameSSP/src/main/java/com/hillel/servis/impl/RecordingResults.java
@@ -4,31 +4,32 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Locale;
 
 public class RecordingResults {
 
-    public String fileNameGenerator(String name) throws IOException {
+    public String fileNameGenerator(String name){
+
+        String s =  Paths.get("").toAbsolutePath().toString();
         String fileName = "result_" + name.toLowerCase(Locale.ROOT) + "_"  + ".txt";
-
-        Path currentRelativePath = Paths.get("GameSSP/src/main/java/com/hillel/fileResult/");
-        String s = currentRelativePath.toAbsolutePath().toString();
-
         File file = new File(s, File.separator.concat(fileName));
 
         if(!file.exists()){
-            file.createNewFile();
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
         return s + File.separator.concat(fileName);
     }
 
-    public void appendToFile(File file, String content)
-            throws IOException {
-
+    public void appendToFile(File file, String content){
         try (FileOutputStream fos = new FileOutputStream(file, true)) {
             fos.write(content.getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
